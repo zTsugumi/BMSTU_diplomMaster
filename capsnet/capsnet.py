6208595174eb5296915d7e126f706cb4c57e73c3
zTsugumi <nguyenngochai.tin47@gmail.com>
2021-11-19 16:00:14 +0300

    Clean up code

--- capsnet/capsnet.py
+++ capsnet/capsnet.py
@@ -5,39 +5,39 @@ from capsnet.gen_model import build_graph
 
 
 class CapsNet(Model):
-    def __init__(self, name, mode, conf_path='conf.json', r=3):
-        Model.__init__(self, name, mode, conf_path)
-        self.load_conf()
+  def __init__(self, name, mode, conf_path='conf.json', r=3):
+    Model.__init__(self, name, mode, conf_path)
+    self.load_conf()
 
-        self.dir_model = self.conf['dir_log'] + f'/capsnet_{name}'
-        self.dir_log = self.conf['dir_log'] + f'/capsnet_{name}'
+    self.dir_model = self.conf['dir_log'] + f'/capsnet_{name}'
+    self.dir_log = self.conf['dir_log'] + f'/capsnet_{name}'
 
-        self.model = build_graph(
-            name, self.conf[f'input_{name}'], self.conf[f'class_{name}'], self.mode, r)
+    self.model = build_graph(
+        name, self.conf[f'input_{name}'], self.conf[f'class_{name}'], self.mode, r)
 
-    def train(self, dataset, initial_epoch=0):
-        data_train, data_test = dataset.get_tf_data()
+  def train(self, dataset, initial_epoch=0):
+    data_train, data_val, _ = dataset.get_tf_data()
 
-        cp, tb = checkpoint(self.dir_model, self.dir_log)
-        lr = lr_sched(self.conf['lr'], self.conf['lr_decay'])
+    cp, tb = checkpoint(self.dir_model, self.dir_log)
+    lr = lr_sched(self.conf['lr'], self.conf['lr_decay'])
 
-        self.model.compile(
-            optimizer=tf.keras.optimizers.Adam(lr=self.conf['lr']),
-            loss=[margin_loss, 'mse'],
-            loss_weights=[1.0, self.conf['alpha']],
-            metrics={'Encoder': 'accuracy'}
-        )
+    self.model.compile(
+        optimizer=tf.keras.optimizers.Adam(lr=self.conf['lr']),
+        loss=[margin_loss, 'mse'],
+        loss_weights=[1.0, self.conf['alpha']],
+        metrics={'Encoder': 'accuracy'}
+    )
 
-        if initial_epoch > 0:
-            self.load_weight(initial_epoch)
+    if initial_epoch > 0:
+      self.load_weight(initial_epoch)
 
-        history = self.model.fit(
-            data_train,
-            validation_data=(data_test),
-            epochs=self.conf['epochs'],
-            batch_size=self.conf['batch_size'],
-            initial_epoch=initial_epoch,
-            callbacks=[cp, tb, lr]
-        )
+    history = self.model.fit(
+        data_train,
+        validation_data=(data_val),
+        epochs=self.conf['epochs'],
+        batch_size=self.conf['batch_size'],
+        initial_epoch=initial_epoch,
+        callbacks=[cp, tb, lr]
+    )
 
-        return history
+    return history
--- capsnet_mod/gen_model.py
+++ capsnet_mod/gen_model.py
@@ -2,32 +2,6 @@ import numpy as np
 import tensorflow as tf
 from capsnet_mod.layers import PrimaryCaps, DigitCaps, Length, Mask
 
-params_MNIST = {
-    'name': 'MNIST',
-    'conv_kernel': 9,
-    'conv_stride': 1,
-
-    'caps_primary': 32,
-    'caps_primary_dim': 8,
-    'caps_primary_kernel': 9,
-    'caps_primary_stride': 1,
-
-    'caps_digit_dim': 16
-}
-
-params_SMALLNORB = {
-    'name': 'SMALLNORB',
-    'conv_kernel': 9,
-    'conv_stride': 1,
-
-    'caps_primary': 32,
-    'caps_primary_dim': 8,
-    'caps_primary_kernel': 9,
-    'caps_primary_stride': 2,
-
-    'caps_digit_dim': 16
-}
-
 params_CIFAR10 = {
     'name': 'CIFAR10',
     'conv_kernel': 9,
@@ -43,270 +17,174 @@ params_CIFAR10 = {
 
 
 def encoder_graph(params, input_shape, output_class):
-    '''
-    This constructs the Encoder layers of Modified Capsule Network
-    '''
-    inputs = tf.keras.Input(input_shape)
+  '''
+  This constructs the Encoder layers of Modified Capsule Network
+  '''
+  inputs = tf.keras.Input(input_shape)
 
-    if params == params_MNIST:
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=5,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(inputs)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+  if params == params_CIFAR10:
+    x = tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=3,
+        strides=1,
+        padding='valid',
+        kernel_initializer='he_normal',
+        activation=None)(inputs)
+    x = tf.keras.layers.LeakyReLU()(x)
+    x = tf.keras.layers.BatchNormalization()(x)
 
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=3,
+        strides=1,
+        padding='valid',
+        kernel_initializer='he_normal',
+        activation=None)(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+    x = tf.keras.layers.BatchNormalization()(x)
 
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Conv2D(
+        filters=128,
+        kernel_size=3,
+        strides=1,
+        padding='valid',
+        kernel_initializer='he_normal',
+        activation=None)(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+    x = tf.keras.layers.BatchNormalization()(x)
 
-        x = tf.keras.layers.Conv2D(
-            filters=256,
-            kernel_size=3,
-            strides=2,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-    elif params == params_SMALLNORB:
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=5,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(inputs)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Conv2D(
+        filters=256,
+        kernel_size=3,
+        strides=1,
+        padding='valid',
+        kernel_initializer='he_normal',
+        activation=None)(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+    x = tf.keras.layers.BatchNormalization()(x)
 
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512,
+        kernel_size=3,
+        strides=2,
+        padding='valid',
+        kernel_initializer='he_normal',
+        activation=None)(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+  else:
+    raise RuntimeError('model not recognized')
 
-        x = tf.keras.layers.Conv2D(
-            filters=128,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+  primary_caps = PrimaryCaps(
+      C=params['caps_primary'],
+      L=params['caps_primary_dim'],
+      k=params['caps_primary_kernel'],
+      s=params['caps_primary_stride'])(x)
+  digit_caps = DigitCaps(
+      C=output_class,
+      L=params['caps_digit_dim'])(primary_caps)
+  digit_caps_len = Length()(digit_caps)
 
-        x = tf.keras.layers.Conv2D(
-            filters=256,
-            kernel_size=3,
-            strides=2,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-    elif params == params_CIFAR10:
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(inputs)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=128,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=256,
-            kernel_size=3,
-            strides=1,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=512,
-            kernel_size=3,
-            strides=2,
-            padding='valid',
-            kernel_initializer='he_normal',
-            activation=None)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-    else:
-        raise RuntimeError('model not recognized')
-
-    primary_caps = PrimaryCaps(
-        C=params['caps_primary'],
-        L=params['caps_primary_dim'],
-        k=params['caps_primary_kernel'],
-        s=params['caps_primary_stride'])(x)
-    digit_caps = DigitCaps(
-        C=output_class,
-        L=params['caps_digit_dim'])(primary_caps)
-    digit_caps_len = Length()(digit_caps)
-
-    return tf.keras.Model(
-        inputs=inputs,
-        outputs=[primary_caps, digit_caps, digit_caps_len],
-        name='Encoder'
-    )
+  return tf.keras.Model(
+      inputs=inputs,
+      outputs=[primary_caps, digit_caps, digit_caps_len],
+      name='Encoder'
+  )
 
 
-def decoder_graph(params, input_shape, output_class):
-    '''
-    This constructs the Decoder layers
-    '''
-    inputs = tf.keras.Input(
-        params['caps_digit_dim']
-    )
+def decoder_graph(params):
+  '''
+  This constructs the Decoder layers
+  '''
+  inputs = tf.keras.Input(
+      params['caps_digit_dim']
+  )
 
-    if params == params_MNIST:
-        x = tf.keras.layers.Dense(784, activation='relu')(inputs)
-        x = tf.keras.layers.Reshape((7, 7, 16))(x)
-    elif params == params_SMALLNORB:
-        x = tf.keras.layers.Dense(1024, activation='relu')(inputs)
-        x = tf.keras.layers.Reshape((8, 8, 16))(x)
-    elif params == params_CIFAR10:
-        x = tf.keras.layers.Dense(1024, activation='relu')(inputs)
-        x = tf.keras.layers.Reshape((8, 8, 16))(x)
-    else:
-        raise RuntimeError(f'model not recognized')
+  if params == params_CIFAR10:
+    x = tf.keras.layers.Dense(1024, activation='relu')(inputs)
+    x = tf.keras.layers.Reshape((8, 8, 16))(x)
+  else:
+    raise RuntimeError(f'model not recognized')
 
-    x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-    x = tf.keras.layers.Conv2DTranspose(64, 3, (1, 1), padding='same')(x)
-    x = tf.keras.layers.Conv2DTranspose(32, 3, (2, 2), padding='same')(x)
-    x = tf.keras.layers.Conv2DTranspose(16, 3, (2, 2), padding='same')(x)
-    x = tf.keras.layers.Conv2DTranspose(8, 3, (1, 1), padding='same')(x)
+  x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
+  x = tf.keras.layers.Conv2DTranspose(64, 3, (1, 1), padding='same')(x)
+  x = tf.keras.layers.Conv2DTranspose(32, 3, (2, 2), padding='same')(x)
+  x = tf.keras.layers.Conv2DTranspose(16, 3, (2, 2), padding='same')(x)
+  x = tf.keras.layers.Conv2DTranspose(8, 3, (1, 1), padding='same')(x)
 
-    if params == params_MNIST:
-        x = tf.keras.layers.Conv2DTranspose(1, 3, (1, 1), padding='same')(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.Reshape((28, 28, 1))(x)
-    elif params == params_SMALLNORB:
-        x = tf.keras.layers.Conv2DTranspose(2, 3, (1, 1), padding='same')(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.Reshape((32, 32, 2))(x)
-    elif params == params_CIFAR10:
-        x = tf.keras.layers.Conv2DTranspose(3, 3, (1, 1), padding='same')(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.Reshape((32, 32, 3))(x)
-    else:
-        raise RuntimeError(f'model not recognized')
+  if params == params_CIFAR10:
+    x = tf.keras.layers.Conv2DTranspose(3, 3, (1, 1), padding='same')(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    x = tf.keras.layers.Reshape((32, 32, 3))(x)
+  else:
+    raise RuntimeError(f'model not recognized')
 
-    return tf.keras.Model(
-        inputs=inputs,
-        outputs=x,
-        name='Decoder'
-    )
+  return tf.keras.Model(
+      inputs=inputs,
+      outputs=x,
+      name='Decoder'
+  )
 
 
-def build_graph(name, input_shape, output_class, mode):
-    '''
-    This contructs the whole architecture of Capsule Network 
-    (Encoder + Decoder)
-    '''
-    # Setup params
-    if name == 'MNIST':
-        params = params_MNIST
-    elif name == 'SMALLNORB':
-        params = params_SMALLNORB
-    elif name == 'CIFAR10':
-        params = params_CIFAR10
-    else:
-        raise RuntimeError(f'name {name} not recognized')
+def build_graph(name, input_shape, output_class, mode, verbose=False):
+  '''
+  This contructs the whole architecture of Capsule Network 
+  (Encoder + Decoder)
+  '''
+  # Setup params
+  if name == 'CIFAR10':
+    params = params_CIFAR10
+  else:
+    raise RuntimeError(f'name {name} not recognized')
 
-    # Encoder
-    inputs = tf.keras.Input(input_shape)
-    y_true = tf.keras.Input(output_class)
+  # Encoder
+  inputs = tf.keras.Input(input_shape)
+  y_true = tf.keras.Input(output_class)
 
-    encoder = encoder_graph(params, input_shape, output_class)
+  encoder = encoder_graph(params, input_shape, output_class)
 
-    primary_caps, digit_caps, digit_caps_len = encoder(inputs)
+  primary_caps, digit_caps, digit_caps_len = encoder(inputs)
 
+  if verbose:
     encoder.summary()
 
-    # Decoder
-    if mode == 'train':
-        masked = Mask()([digit_caps, y_true])
-    elif mode == 'test':
-        masked = Mask()(digit_caps)
-    elif mode == 'exp':
-        noise = tf.keras.Input(
-            (output_class, params['caps_digit_dim']))
-        digit_caps_noise = tf.keras.layers.add([digit_caps, noise])
-        masked = Mask()(digit_caps_noise)
-    else:
-        raise RuntimeError(f'mode {mode} not recognized')
+  # Decoder
+  if mode == 'train':
+    masked = Mask()([digit_caps, y_true])
+  elif mode == 'test':
+    masked = Mask()(digit_caps)
+  elif mode == 'exp':
+    noise = tf.keras.Input(
+        (output_class, params['caps_digit_dim']))
+    digit_caps_noise = tf.keras.layers.add([digit_caps, noise])
+    masked = Mask()(digit_caps_noise)
+  else:
+    raise RuntimeError(f'mode {mode} not recognized')
 
-    decoder = decoder_graph(params, input_shape, output_class)
+  decoder = decoder_graph(params)
 
-    x_reconstruct = decoder(masked)
+  x_reconstruct = decoder(masked)
 
+  if verbose:
     decoder.summary()
 
-    if mode == 'train':
-        return tf.keras.Model(
-            inputs=[inputs, y_true],
-            outputs=[digit_caps_len, x_reconstruct],
-            name='CapsNetMod'
-        )
-    elif mode == 'test':
-        return tf.keras.Model(
-            inputs=[inputs],
-            outputs=[digit_caps_len, x_reconstruct],
-            name='CapsNetMod'
-        )
-    elif mode == 'exp':
-        return tf.keras.Model(
-            inputs=[inputs, noise],
-            outputs=[digit_caps_len, x_reconstruct],
-            name='CapsNetMod'
-        )
-    else:
-        raise RuntimeError(f'mode {mode} not recognized')
+  if mode == 'train':
+    return tf.keras.Model(
+        inputs=[inputs, y_true],
+        outputs=[digit_caps_len, x_reconstruct],
+        name='CapsNetMod'
+    )
+  elif mode == 'test':
+    return tf.keras.Model(
+        inputs=[inputs],
+        outputs=[digit_caps_len, x_reconstruct],
+        name='CapsNetMod'
+    )
+  elif mode == 'exp':
+    return tf.keras.Model(
+        inputs=[inputs, noise],
+        outputs=[digit_caps_len, x_reconstruct],
+        name='CapsNetMod'
+    )
+  else:
+    raise RuntimeError(f'mode {mode} not recognized')
